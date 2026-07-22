b0deb0d3f714ff7b5dd2279a5917c321251dd49b
Daniels <daniels.developer48@gmail.com>
2026-07-22 23:43:01 +0400

    =22.07

--- app/web/static/js/issue.js
+++ app/web/static/js/issue.js
@@ -55,7 +55,6 @@ const cancelReply = document.getElementById("cancel-reply");
 // const textArea = document.getElementById("text_area");
 
 
-
 btn_edit_issue.addEventListener("click", editIssue);
 btn_close_issue.addEventListener("click", close_issue)
 close_issue_modal.addEventListener("click", close_issue_func);
@@ -63,88 +62,14 @@ cancel_issue_form.addEventListener("click", reset_issue_form);
 modal_issue.addEventListener("click", modal_issue_func);
 i_apply.addEventListener("click", applyEdit);
 commentsContainer.addEventListener("click", comments_action);
-cancelReply.addEventListener("click", cancelReplyMode);
-
+cancelReply.addEventListener("click", cancel_reply_comment);
+btn_post_comment.addEventListener("click", post_comment);
 
 back_url.href = back_url.href + projectId;
 
-btn_post_comment.addEventListener("click", post_comment);
-
-async function comments_action(event) {
-    const btn = event.target;
-
-    const get_comment_container = value => value.parentElement.parentElement.parentElement;
-    const get_value_comment = value => value.parentElement.previousElementSibling;
-    const get_owner_comment = value => value.parentElement.previousElementSibling.previousElementSibling.firstElementChild;
-    const get_id_comment = value => value.dataset.id;
-
-    function get_data_comment(btn) {
-        const container = get_comment_container(btn)
-        const id_comment = get_id_comment(container)
-        const obj_comment = get_value_comment(btn)
-        const owner_comment = get_owner_comment(btn)
-
-        return {
-            id: id_comment,
-            value: obj_comment.textContent,
-            owner: owner_comment.textContent,
-        }
-    }
 
 
-    if (btn.classList.contains("btn_reply")) {
-        console.log("reply");
-        const data = get_data_comment(btn)
-
-        function startReply(data) {
-            // replyingTo = comment.public_id;
-            //
-            // commentTitle.textContent = "Reply";
-
-            replyAuthor.textContent = data.owner;
-            replyPreview.textContent = data.value;
-            text_area_comment.dataset.id = data.id;
-
-            replyInfo.classList.remove("hidden");
-
-            // postComment.textContent = "Post Reply";
-
-            text_area_comment.focus();
-        }
-
-        startReply(data);
-    }
-    if (btn.classList.contains("btn_edit")) {
-        console.log("edit");
-        const data = get_data_comment(btn)
-        console.log(data)
-    }
-    if (btn.classList.contains("btn_delete")) {
-        console.log("del");
-        const data = get_data_comment(btn)
-        const res = await api.del(window.data_url.comments(projectId,IssueId, data.id));
-        if (!res || !res.ok) {return;}
-        if (res.status === 204) {
-            const element = document.querySelector(`[data-id="${data.id}"]`);
-            if (element) {element.remove();}
-        }
-    }
-}
-
-function cancelReplyMode() {
-    // replyingTo = null;
-    //
-    // commentTitle.textContent = "Add Comment";
-
-    replyInfo.classList.add("hidden");
-
-    // postComment.textContent = "Post Comment";
-
-    replyAuthor.textContent = "";
-    replyPreview.textContent = "";
-    text_area_comment.dataset.id = null;
-}
-
+// ---------------------------------------Head----------------------------------------------
 async function loadIssue() {
     const res = await api.get(window.data_url.issue(projectId, IssueId));
     if (!res || !res.ok) {
@@ -222,11 +147,12 @@ function renderIssueDetails(issue, offload=false) {
 
 function renderComments(comments) {
     if (comments.length === 0) {
-        commentsContainer.innerHTML = commentsContainer.innerHTML + `<div class="empty">No comments</div>`;
+        const empty_text = `<div class="empty">No comments</div>`;
+        commentsContainer.insertAdjacentHTML("beforeend", empty_text);
         return;
     }
 
-    commentsContainer.innerHTML = commentsContainer.innerHTML + renderTree(comments);
+    commentsContainer.insertAdjacentHTML("beforeend", renderTree(comments));
 
     function renderTree(comments, level = 0, parent = null) {
     let html = ``;
@@ -237,29 +163,69 @@ function renderComments(comments) {
     return html;
 }
 
+    function childrenAttribute(comment) {
+        if (!comment.children.length) {return "";}
+        return `data-list-ids='${JSON.stringify(getChildrenIds(comment))}'`;
+    }
+
+    function getChildrenIds(comment) {
+        const ids = [];
+
+        function walk(node) {
+            for (const child of node.children) {
+                ids.push(child.public_id);
+                walk(child);
+            }
+        }
+
+        walk(comment);
+
+        return ids;
+    }
+
     function commentHtml(comment, level, parent) {
         const visualLevel = Math.min(level, 3);
 
+        const comments_edit = comment => {
+            return `<div class="comment-edit hidden">
+                        <textarea class="comment-area">${comment.content}</textarea>
+                        <div class="edit-actions">
+                            <button class="btn-save">Save</button>
+                            <button class="btn-cancel">Cancel</button>
+                        </div>
+                    </div>`
+        }
+
+        const comments_head = comment => {
+            return `<div class="comment-head">
+                        <p class="content-comment" data-text="${comment.content}">${comment.content}</p>
+                        <div class="comment-meta">
+                            edited 2 minutes ago
+                        </div>
+                        <div class="comment-actions">
+                            <button class="btn_reply">Reply</button>
+                            <button class="btn_edit">Edit</button>
+                            <button class="btn_delete">Delete</button>
+                        </div>
+                    </div>`
+        }
+
+        let z = `<button class="toggle-replies">▼ 4 replies</button>`
+
         return `
-            <article data-id="${comment.public_id}" class="comment level-${visualLevel} card">
+            <article data-id="${comment.public_id}" ${childrenAttribute(comment)} class="comment level-${visualLevel} card">
                 <div class="avatar">${getInitial(comment.author.username)}</div>
                 <div class="comment-content">
-                   ${parent ? commentReply(parent) : ""}
                     <div class="comment-header">
-                        <strong>${comment.author.username}</strong>
-                        <span>${formatDateTime(comment.created_at)}</span>
+                        <strong class="comment-owner" data-owner="${comment.author.username}">${comment.author.username}</strong>
+                        <span class="comment-create-date">${formatDateTime(comment.created_at)}</span>
                     </div>
-                    <p>${comment.content}</p>
-                    <div class="comment-actions">
-                        <button class="btn_reply">Reply</button>
-                        <button class="btn_edit">Edit</button>
-                        <button class="btn_delete">Delete</button>
-                    </div>
+                    ${parent ? commentReply(parent) : ""}
+                    ${comments_head(comment)}
+                    ${comments_edit(comment)}
                 </div>
             </article>
-            <button class="toggle-replies">
-                ▼ 4 replies
-            </button>
+
         `;
     }
 
@@ -274,21 +240,17 @@ function renderComments(comments) {
 
     function formatDateTime(dateString) {
         if (!dateString) return "—";
-
         const date = new Date(dateString);
-
         const datePart = new Intl.DateTimeFormat("en-US", {
             month: "short",
             day: "numeric",
             year: "numeric",
         }).format(date);
-
         const timePart = new Intl.DateTimeFormat("en-US", {
             hour: "2-digit",
             minute: "2-digit",
             hour12: false,
         }).format(date);
-
         return `${datePart} • ${timePart}`;
     }
 
@@ -297,27 +259,122 @@ function renderComments(comments) {
         return text.charAt(0).toUpperCase();
     }
 }
+// -----------------------------------------------------------------------------------------
+
+
+// ------------------------------------Comments Action--------------------------------------
+async function comments_action(event) {
+    const btn = event.target;
+    const container = btn.closest("article.comment");
+    if (!container) {return;}
+    const obj_comment = container.querySelector("p.content-comment");
+    const owner_comment = container.querySelector("strong.comment-owner");
+
+    const data = {
+        id: container.dataset.id,
+        value: obj_comment.dataset.text,
+        owner: owner_comment.dataset.owner,
+        container: container,
+    }
+
+    if (btn.classList.contains("btn_reply")) {reply_comment(data);}
+    if (btn.classList.contains("btn_edit")) {edit_comment(data);}
+    if (btn.classList.contains("btn_delete")) {await delete_comment(data);}
+    if (btn.classList.contains("btn-cancel")) {close_edit_comment(data);}
+    if (btn.classList.contains("btn-save")) {save_edit_comment(data);}
+
+}
+
+function get_containers_edit(data) {
+    const head_container = data.container.querySelector(".comment-head");
+    const edit_container = data.container.querySelector(".comment-edit");
+
+    return {
+        head_container: head_container,
+        edit_container: edit_container
+    }
+}
+
+function edit_comment(data) {
+    const containers = get_containers_edit(data);
+    containers.head_container.classList.add("hidden");
+    containers.edit_container.classList.remove("hidden");
+
+    const textarea = data.container.querySelector("textarea.comment-area");
+    textarea.value = data.value;
+    textarea.focus();
+    textarea.setSelectionRange(textarea.value.length, textarea.value.length);
+
+
+    function autoResize(textarea) {
+        textarea.style.height = "0";
+        textarea.style.height = textarea.scrollHeight + "px";
+    }
+
+}
+
+function close_edit_comment(data) {
+    const containers = get_containers_edit(data);
+    containers.head_container.classList.remove("hidden");
+    containers.edit_container.classList.add("hidden");
+}
+
+function save_edit_comment(data) {
+    console.log(commentsContainer.children.length - 1);
+}
+
+function reply_comment(data) {
+    replyAuthor.textContent = data.owner;
+    replyPreview.textContent = data.value;
+    text_area_comment.dataset.id = data.id;
+    replyInfo.classList.remove("hidden");
+    text_area_comment.focus();
+}
+
+function cancel_reply_comment() {
+    replyInfo.classList.add("hidden");
+    replyAuthor.textContent = "";
+    replyPreview.textContent = "";
+    text_area_comment.dataset.id = null;
+}
+
+async function delete_comment(data) {
+        const res = await api.del(window.data_url.comments(projectId,IssueId, data.id));
+        if (!res || !res.ok) {return;}
+        if (res.status === 204) {
+            alert("Delete Complete!")
+            const element = document.querySelector(`[data-id="${data.id}"]`);
+            if (element) {
+                if (element?.dataset.listIds) {
+                    const ids = JSON.parse(element.dataset.listIds);
+                    for (const id of ids) {
+                        const comment = document.querySelector(`[data-id="${id}"]`);
+                        if (comment) {comment.remove()}
+                    }
+                }
+                element.remove();
+                const count = commentsContainer.children.length - 1;
+                len_comments.innerText = `${count} comment${count === 1 ? "" : "s"}`;
+
+            }
+        }
+}
 
 async function post_comment(event) {
-    const id_reply = text_area_comment.dataset.id;
-    const data = {content: text_area_comment.value, parent_comment_public_id: id_reply}
-    const res = await api.post(window.data_url.comment(projectId,IssueId), data);
-    if (!res || !res.ok) {return;}
-    const comment = await res.json();
+    if (text_area_comment.value.length > 1) {
+        const id_reply = text_area_comment.dataset.id;
+        const data = {content: text_area_comment.value, parent_comment_public_id: id_reply}
+        const res = await api.post(window.data_url.comment(projectId,IssueId), data);
+        if (!res || !res.ok) {return;}
+        const comment = await res.json();
+        text_area_comment.value = ""
+    }
 }
+// -----------------------------------------------------------------------------------------
 
-function formatEnum(value) {
-    return value
-        .replaceAll("_", " ")
-        .replace(/\b\w/g, c => c.toUpperCase());
-}
 
-function unformatEnum(value) {
-    return value
-        .toLowerCase()
-        .replaceAll(" ", "_");
-}
 
+// ----------------------------------------Issue Action-------------------------------------
 async function editIssue(event) {
     const response = await window.api.get(window.data_url.issueEdit(projectId, IssueId));
     if (!response.ok) {return;}
@@ -399,7 +456,20 @@ function modal_issue_func(event) {
        form_issue.reset();
    }
 }
+// -----------------------------------------------------------------------------------------
 
+
+function formatEnum(value) {
+    return value
+        .replaceAll("_", " ")
+        .replace(/\b\w/g, c => c.toUpperCase());
+}
+
+function unformatEnum(value) {
+    return value
+        .toLowerCase()
+        .replaceAll(" ", "_");
+}
 
 
 loadIssue();
